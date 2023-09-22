028808d1022fd623801022cb71f1a33276f431a3
Julian Toledo <84879981+juliantoledo94@users.noreply.github.com>
2023-09-22 09:22:47 -0300

    feat: add localStorage function

--- scripts/app.js
+++ scripts/app.js
@@ -1,64 +1,11 @@
-/* 
-
-Vue.component("form-contraseña",{
-    template:`  <div>
-                <h2>Agregar Contraseña</h2>
-                <form v-on: submit.prevent>
-                <div>
-                    <label for="accountType">Tipo de Cuenta:</label>
-                    <select v-model="selectedAccountType" id="accountType">
-                    <option value="gmail">Gmail</option>
-                    <option value="hotmail">Hotmail</option>
-                    </select>
-                </div>
-                <div>
-                    <label for="platform">Plataforma:</label>
-                    <input type="text" v-model="platform" id="platform" />
-                </div>
-                <div>
-                    <label for="password">Contraseña:</label>
-                    <input type="password" v-model="password" id="password" />
-                </div>
-                <div>
-                    <button type="submit">Guardar Contraseña</button>
-                </div>
-                </form>
-            </div>  
-    `,
-    data:function(){
-        return {
-            selectedAccountType: 'gmail',
-            platform: '',
-            password: '', // Agregamos una propiedad para la contraseña
-          };
-        },
-        methods: {
-          savePassword() {
-            const passwordData = {
-              accountType: this.selectedAccountType,
-              platform: this.platform,
-              password: this.password, // Agregamos la contraseña al objeto
-            };
-            // Guardar en localStorage
-            localStorage.setItem('password', JSON.stringify(passwordData));
-            this.selectedAccountType = 'gmail';
-            this.platform = '';
-            this.password = ''; // Limpiar la contraseña después de guardar
-          },
-        },
-});
-
-
-
-*/
 
 Vue.component("form-contraseña",{
   template:`  <div>
             <h2 class="d-flex justify-content-center m-3 pt-3">Agregar Contraseña</h2>
-            <form v-on: submit.prevent>
+            <form v-on:submit.prevent>
             <div>
                 <label for="accountType">Tipo de Cuenta:</label>
-                <select v-model="selectedAccountType" id="accountType">
+                <select v-model="dato.selectedAccountType" id="accountType">
                 <option value="gmail">Gmail</option>
                 <option value="netflix">Netflix</option>
                 <option value="steam">Steam</option>
@@ -67,25 +14,48 @@ Vue.component("form-contraseña",{
             </div>
             <div>
                 <label >Usuario:</label>
-                <input type="text" v-model="user"/>
+                <input type="text" v-model="dato.user"/>
             </div>
             <div>
                 <label>Contraseña:</label>
-                <input type="password" v-model="password" />
+                <input type="password" v-model="dato.password" />
             </div>
             <div>
-                <button type="submit">Guardar Contraseña</button>
+                <button @click="guardar(dato)">Guardar Contraseña</button>
             </div>
             </form>
           </div>  
 `,
   data:function(){
     return {
-        selectedAccountType: 'gmail',
-        user: '',
-        password: '', // Agregamos una propiedad para la contraseña
+        dato:{
+          selectedAccountType: 'gmail',
+          user: '',
+          password: '',
+        },
+        arr:[],
+        
       };
     },
+  methods:{
+    guardar(dato){
+      console.log(dato)
+      if (!dato.selectedAccountType || !dato.user || !dato.password) {
+        // Si falta algún dato requerido, muestra un mensaje de error o realiza alguna acción apropiada.
+        alert('Por favor, complete todos los campos.');
+        return;
+      }
+      if(!localStorage.local){
+        this.arr=[]
+      }else{
+        this.arr=JSON.parse(localStorage.getItem("local"))
+        }
+
+      this.arr.push(dato)
+      localStorage.setItem("local",JSON.stringify(this.arr))
+      this.dato={selectedAccountType:"",user:"",password:""}
+    }
+  }
     
 })
 
